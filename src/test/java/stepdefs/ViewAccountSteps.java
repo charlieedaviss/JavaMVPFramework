848e42f8650e401fb269535a2129bef770f61300
maggiee0606 <maggiee0606@yahoo.com>
2023-03-02 21:40:39 -0600

    "confirming account info stored in database"

--- src/test/java/stepdefs/ViewAccountSteps.java
+++ src/test/java/stepdefs/ViewAccountSteps.java
@@ -2,6 +2,10 @@ package stepdefs;
 
 import io.cucumber.java.en.Then;
 import pages.ViewAccountPage;
+import utilities.DatabaseUtils;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class ViewAccountSteps {
 
@@ -13,14 +17,22 @@ public class ViewAccountSteps {
 
 
     }
+
     @Then("verify transaction history displayed for the new transaction")
     public void verify_transaction_history_displayed_for_the_new_transaction() {
         viewAccountPage.verifyTransactionHistor();
 
     }
+
     @Then("verify transaction details getting updated into the database")
     public void verify_transaction_details_getting_updated_into_the_database() {
         viewAccountPage.verifyDetailsIntoDatabase();
 
     }
-}
+
+    @Then("verify account has been stored in database")
+    public void verifyAccountHasBeenStoredInDatabase() {
+       viewAccountPage.verifyDetailsIntoDatabase();
+        }
+    }
+
